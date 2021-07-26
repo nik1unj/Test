736d76695bad4ce029c03b4f4400031dd25cfad9
Nikunj <patelnikunj21429@gmail.com>
2021-07-26 15:35:00 +0530

    test

---
+++ Test.java
@@ -0,0 +1,28 @@
+package com.nsr.handlers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import com.gtranslate.Language;
+import com.gtranslate.Translator;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+       // String text = "Hello world!";
+       
+        Translator translate = Translator.getInstance();
+        String text = translate.translate("Hello!", Language.ENGLISH, Language.ROMANIAN);
+        System.out.println(text);
+		System.out.println(text);
+		System.out.println(text);
+		System.out.println(text);
+		System.out.println(text);
+    }
+
+
+}
